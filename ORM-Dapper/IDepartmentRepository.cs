42b6e679716077a3dfc6dd0ee9e980072e65caec
samanthakyra <samanthakyramendoza@gmail.com>
2022-10-11 08:56:10 -0700

    initial commit

---
+++ ORM-Dapper/IDepartmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ORM_Dapper
+{
+    public interface IDepartmentRepository
+    {
+        public IEnumerable<Department> GetAllDepartments();
+        public void InsertDepartment(string newDepartmentName);
+    }
+}
+
